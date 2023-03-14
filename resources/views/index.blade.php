5d66541dd43d78b170a1ee3890818affc7c16fb8
Leon14789 <leoortizinho23@gmail.com>
2023-03-14 15:02:43 -0300

    primeiro commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,197 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>AUTO SOCORRO XAVIER</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/media-query.css" />
+		<link rel="stylesheet" href="assets/css/wats.css" />
+
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="homepage is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header">
+
+					<!-- Inner -->
+						<div class="inner">
+							<header>
+								
+								<hr />
+								<p>Acompanhe um pouco da nossa empresa	</p>
+							</header>
+							<footer>
+								<a href="#banner" class="button circled scrolly">Iniciar</a>
+							</footer>
+							<a href="https://wa.me/5515997955461?text=Ol%C3%A1%20Preciso%20de%20um%20Guincho" class="btn-whatsapp-pulse" target="_blank">
+								<i class="fab fa-whatsapp"></i>
+							</a>
+						</div>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li><a href="left-sidebar.html" class="txt">ACESSAR SISTEMA</a></li>
+							</ul>
+						</nav>
+
+				</div>
+
+			<!-- Banner -->
+				<section id="banner">
+					<header>
+						<h2 class="txt">Veja Aqui Nossa Frota de Caminhões</h2>
+					</header>
+				</section>
+
+			<!-- Carousel -->
+				<section class="carousel">
+					<div class="reel">
+
+						<article>
+							<a href="https://wa.me/5515997955461?text=Ol%C3%A1%20Preciso%20de%20um%20Guincho" class="image featured"><img src="assets/images/frota/pic01.jpg" alt="" /></a>
+						</article>
+
+						<article>
+							<a href="https://wa.me/5515997955461?text=Ol%C3%A1%20Preciso%20de%20um%20Guincho" class="image featured"><img src="assets/images/frota/pic02.jpg" alt="" /></a>
+						</article>
+
+						<article>
+							<a href="https://wa.me/5515997955461?text=Ol%C3%A1%20Preciso%20de%20um%20Guincho" class="image featured"><img src="assets/images/frota/pic03.jpg" alt="" /></a>
+						</article>
+
+						<article>
+							<a href="https://wa.me/5515997955461?text=Ol%C3%A1%20Preciso%20de%20um%20Guincho" class="image featured"><img src="assets/images/frota/pic04.jpg" alt="" /></a>
+						</article>
+
+						<article>
+							<a href="https://wa.me/5515997955461?text=Ol%C3%A1%20Preciso%20de%20um%20Guincho" class="image featured"><img src="assets/images/frota/pic05.jpg" alt="" /></a>
+						</article>
+
+						<article>
+							<a href="https://wa.me/5515997955461?text=Ol%C3%A1%20Preciso%20de%20um%20Guincho" class="image featured"><img src="assets/images/frota/pic06.jpg" alt="" /></a>
+						</article>
+
+						<article>
+							<a href="https://wa.me/5515997955461?text=Ol%C3%A1%20Preciso%20de%20um%20Guincho" class="image featured"><img src="assets/images/frota/pic07.jpg" alt="" /></a>
+						</article>
+
+					</div>
+				</section>
+
+			<!-- Main -->
+				<div class="wrapper style2">
+
+					<article id="main" class="container special">
+						<header>
+							<h2 class="txt-quem">Quem Somos?</h2>
+							<p>
+								Somos uma empresa prestadora de guincho,na qual trabalhamos com um alto padrão de atendimento aos nossos clientes. somos uma das Prestadora de guincho mais tradicionais da cidade de São Paulo.
+								Atualmente estamos localizados Rua floripes dias pagliato 35 jardim bethania mas realizamos atendimento em toda região de SP
+							</p>
+						</header>
+						<video src="assets/images/video/video.mp4" class="video" controls></video>
+					</article>
+
+				</div>
+
+			<!-- Features -->
+				<div class="wrapper style1">
+
+					<section id="features" class="container special">
+						<header>
+							<h2 class="txt"> Seguradoras Parceiras</h2>
+							<p>Analizando as fotos anexada podera averiguar alguma das empresas que conosco possuiem parceiria isto é: seguradoras que nós realizamos o atendimento </p>
+						</header>
+						<div class="row">
+							<article class="col-4 col-12-mobile special">
+								<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjV7qvrkMr9AhXIr5UCHcfYAU0QFnoECA4QAQ&url=https%3A%2F%2Fwww.hdiseguros.com.br%2F&usg=AOvVaw2Y--ArNAHat9pqDBleyk2J" class="" target="_blank"><img src="assets/images/logos/hdi.png" alt="" /></a>
+								
+							</article>
+							
+							<article class="col-4 col-12-mobile special">
+								<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiZzuTUkMr9AhU5jZUCHTvoBucQFnoECAkQAQ&url=https%3A%2F%2Fwww.facilassist.com.br%2F&usg=AOvVaw140dT2V_az7HfzbLYLm73l	"  target="_blank"><img src="assets/images/logos/facil.png" alt=""/></a>
+								
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjNpJGymsr9AhUJp5UCHffhAcgQFnoECA4QAQ&url=https%3A%2F%2Fwww.mapfre.com.br%2Fquem-somos%2Fnoticias%2Fbrasil-assistencia-agora-e-mapfre-assistencia%2F&usg=AOvVaw2brtc2pgrk3LqINmdKck2K"  target="_blank"><img src="assets/images/logos/brasil.png" alt="" ></a>
+								
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi0ouDNk8r9AhWRpZUCHd_YAaoQFnoECAwQAQ&url=https%3A%2F%2Fwww.unicoon.com.br%2F&usg=AOvVaw0vKP-PU_rEwp6mo0v-q5OQ"  target="_blank"><img src="assets/images/logos/unicom.png" alt="" /></a>
+								
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjF5_L2kMr9AhV4qJUCHZ6KAP4QFnoECAoQAQ&url=https%3A%2F%2Fwww.bemprotege.com.br%2F&usg=AOvVaw1IlX0k5EmRVYSWgYjoXJw5"  target="_blank"><img src="assets/images/logos/bem-protege.png" alt="" /></a>
+								
+							</article>
+
+							<article class="col-4 col-12-mobile special">
+								<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwikyIvukMr9AhVtqpUCHQZKBnUQFnoECAwQAQ&url=https%3A%2F%2Fwww.movida.com.br%2F&usg=AOvVaw1zoB2pjlMIorrO1CXJV1PC"  target="_blank"><img src="assets/images/logos/movida.png" alt="" /></a>
+								
+							</article>
+
+
+							<article class="col-4 col-12-mobile special">
+								<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiY4vj9kMr9AhXDqpUCHSF3AooQFnoECAwQAQ&url=https%3A%2F%2Fwww.allianztravel.com.br%2Fallianz-global-assistance-agora-e-allianz-travel&usg=AOvVaw0bFNiRognOkHFTs1BkWw3U"  target="_blank"><img src="assets/images/logos/mondial.png" alt="" /></a>
+							</article>
+							
+							<article class="col-4 col-12-mobile special">
+								<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjGztzwkMr9AhWXqpUCHZgED3oQFnoECAkQAQ&url=https%3A%2F%2Fcarenb.com%2F&usg=AOvVaw0M3v67uazCYUExht04EIX9"  target="_blank"><img src="assets/images/logos/care.png" alt="" /></a>
+								
+							</article>
+							
+							
+							
+						</div>
+					</section>
+
+				</div>
+
+			<!-- Footer -->
+			<div id="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-12">
+							<!-- Contact -->
+								<section class="contact">
+								
+									<ul class="icons">
+										<li><a href="https://instagram.com/xavierguinchos?igshid=YmMyMTA2M2Y=" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="https://wa.me/5515997955461?text=Ol%C3%A1%20Preciso%20de%20um%20Guincho" class="icon brands fa-whatsapp"><span class="label">Whatsapp</span></a></li>
+
+									</ul>
+								</section>
+							<!-- Copyright -->
+								<div class="copyright">
+									<ul class="menu">
+										<li>&copy; Auto Socorro Xavier.</li><li> Design:Leonardo O. Alves</li>
+									</ul>
+								</div>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+						
+					
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
